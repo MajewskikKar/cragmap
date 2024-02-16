2122da70ee48e05ed6966fceb8d239f0c0039c5f
MajewskiKar <majewskikar@gmail.com>
2024-02-16 16:29:13 +0100

    przed przerobieniem utils

--- map/tests.py
+++
@@ -1,9 +0,0 @@
-from map.models import Site
-
-# Get the model's metadata
-model_meta = Site._meta
-
-# Access foreign keys
-for field in model_meta.fields:
-    if field.is_relation and field.many_to_one:
-        print(f"{field.name} is a foreign key to {field.related_model}")
